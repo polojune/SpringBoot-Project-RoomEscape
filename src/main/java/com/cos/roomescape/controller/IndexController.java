4bf127c3a653b26da0bd5802871773248136b90e
jjk <crossfit@a.naver.com>
2020-08-05 14:44:46 +0900

    OAuth2네이버적용

--- src/main/java/com/cos/roomescape/controller/IndexController.java
+++ src/main/java/com/cos/roomescape/controller/IndexController.java
@@ -1,65 +1,77 @@
 package com.cos.roomescape.controller;
 
+import java.util.Iterator;
+
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cos.roomescape.config.auth.PrincipalDetails;
 import com.cos.roomescape.model.User;
 import com.cos.roomescape.repository.UserRepository;
 
 @Controller
 public class IndexController {
-    //dsfa
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	
+
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	
-	
+
 	@GetMapping({ "", "/" })
 	public @ResponseBody String home() {
 		return "인덱스 페이지";
 	}
-	
+
 	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
-	
 
 	@GetMapping("/theme")
 	public String theme() {
 		return "theme";
 	}
-	
 
 	@GetMapping("/book")
 	public String book() {
 		return "book";
 	}
-	
-	
+
 	@GetMapping("/storeDetail")
 	public String storeDetail() {
 		return "storeDetail";
 
 	}
-	
-	
+
 	@GetMapping("/map")
 	public String map() {
 		return "map";
 	}
 
-
+//	@GetMapping("/user")
+//	public @ResponseBody String user() {
+//		return "유저 페이지";
+//	}
 	@GetMapping("/user")
-	public @ResponseBody String user() {
-		return "유저 페이지";
+	public @ResponseBody String user(@AuthenticationPrincipal PrincipalDetails principal) {
+		System.out.println("Principal : " + principal);
+		System.out.println("OAuth2 : " + principal.getUser().getProvider());
+		// iterator 순차 출력 해보기
+		Iterator<? extends GrantedAuthority> iter = principal.getAuthorities().iterator();
+		while (iter.hasNext()) {
+			GrantedAuthority auth = iter.next();
+			System.out.println(auth.getAuthority());
+		}
+
+		return "유저 페이지입니다.";
 	}
 
 	@GetMapping("/admin")
@@ -71,24 +83,22 @@ public class IndexController {
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/join")
 	public String join() {
 		return "join";
 	}
-	
+
 	@GetMapping("/board")
 	public String board() {
 		return "board";
 	}
-	
 
-	
 	@GetMapping("/store")
 	public String store() {
 		return "store";
 	}
-	
+
 	@PostMapping("/joinProc")
 	public String joinProc(User user) {
 		System.out.println("회원가입 진행 : " + user);
@@ -98,6 +108,6 @@ public class IndexController {
 		user.setRole("ROLE_USER");
 		userRepository.save(user);
 		return "redirect:/";
-	
-	}	
+
+	}
 }
