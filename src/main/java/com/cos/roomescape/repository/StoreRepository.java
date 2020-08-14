ae54553031744e1ad73d6ae25e24b768e58d753e
jjk <crossfit@a.naver.com>
2020-08-14 18:58:50 +0900

    18:58

--- src/main/java/com/cos/roomescape/repository/StoreRepository.java
+++ src/main/java/com/cos/roomescape/repository/StoreRepository.java
@@ -1,3 +1,5 @@
+package com.cos.roomescape.repository;
+
 import java.util.List;
 
 import com.cos.roomescape.dto.ReserveStoreRespDto;
@@ -6,6 +8,7 @@ import com.cos.roomescape.model.Store;
 public interface StoreRepository {
     public List<Store> findAll();
     public Store findById(int storeId);
+    public String findByStoreId(int storeId);
     public List<ReserveStoreRespDto> findByLocation(String location);
     public List<ReserveStoreRespDto> findByAllLocation();
 }
